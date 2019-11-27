f20b3da378ffacdf0dea160c86a6986fd1ea0b60
Vanessa Sochat <vsochat@stanford.edu>
2019-11-27 13:36:45 -0700

    updating pull request logic to save output
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- .github/workflows/pull-request.sh
+++ .github/workflows/pull-request.sh
@@ -49,8 +49,8 @@ create_pull_request() {
     TARGET="${2}"  # pull request TO this target
 
     # Check if the branch already has a pull request open
-    TITLE='Updating Membership Counts'
-    BODY='This is a pull request to update membership counts for usrse.'
+    TITLE='Request for Term Update Review'
+    BODY='This is a pull request to request review for changes to the term README.md.'
     DATA="{\"base\":\"${TARGET}\", \"head\":\"${SOURCE}\", \"body\":\"${BODY}\"}"
     RESPONSE=$(curl -sSL -H "${AUTH_HEADER}" -H "${HEADER}" --user "${GITHUB_ACTOR}" -X GET --data "${DATA}" ${PULLS_URL})
     PR=$(echo "${RESPONSE}" | jq --raw-output '.[] | .head.ref')
@@ -66,7 +66,9 @@ create_pull_request() {
         DATA="{\"title\":\"${TITLE}\", \"base\":\"${TARGET}\", \"head\":\"${SOURCE}\", \"body\":\"${BODY}\"}"
         echo "curl --user ${GITHUB_ACTOR} -X POST --data ${DATA} ${PULLS_URL}"
         curl -sSL -H "${AUTH_HEADER}" -H "${HEADER}" --user "${GITHUB_ACTOR}" -X POST --data "${DATA}" ${PULLS_URL}
-        echo $?
+        response=$?
+        echo $response
+        echo $response > PR_RESPONSE.json
     fi
 }
 
